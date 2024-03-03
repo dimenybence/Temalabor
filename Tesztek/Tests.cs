337f405bb785f90e79622109d41182c24adbc90c
dimenybence <dimeny.bence@gmail.com>
2024-03-03 22:33:17 +0100

    plusz test

--- Tesztek/Tests.cs
+++ Tesztek/Tests.cs
@@ -5,16 +5,16 @@ namespace Tesztek
         [Fact]
         public void FirstCharTest()
         {
-            string input = "alma körte";
-            string expected = "lma örte";
+            string input = "alma körte barack";
+            string expected = "lma örte arack";
             string result = Stringatalakitas.Program.FirstCharacterRemove(input);
             Assert.Equal(expected, result);
         }
         [Fact]
         public void LastCharTest()
         {
-            string input = "alma körte";
-            string expected = "alm kört";
+            string input = "alma körte barack";
+            string expected = "alm kört barac";
             string result = Stringatalakitas.Program.LastCharacterRemove(input);
             Assert.Equal(expected, result);
         }
